7b1f5ff47adc451fd766f0895dd5e60fdd9dacc2
Emmanuel <github-sub@shedge.com>
2023-08-14 16:28:29 +0200

    refactor: update the menu layout so that it's cleaner and easier to use
    refactor: re-organize the functions and add Docstrings

--- demo_clients/demo_client_streamlit_emmanuel/app.py
+++ demo_clients/demo_client_streamlit_emmanuel/app.py
@@ -1,17 +1,17 @@
 import os
 import io
 import requests
-import numpy as np
 
 from PIL import Image
 import cv2
 
 import streamlit as st
-from streamlit_option_menu import option_menu
+
+# ===== VARIABLES =====
 
 DEFAULT_URL = "http://0.0.0.0:5000"
 
-os.environ['OPENCV_FFMPEG_READ_ATTEMPTS'] = "10000"
+os.environ["OPENCV_FFMPEG_READ_ATTEMPTS"] = "10000"
 
 colors = {
     "spatter": "FF0000",  # 921619 ~RED
@@ -29,6 +29,9 @@ colors = {
     "others_not_classified": "0B6B5F",  # 0b6b5f ~ORANGE
     "toe_angle": "327A71",  # 09443d ~DARK GREEN
 }
+
+
+# ===== FUNCTIONS =====
 
 
 def hex_to_rgb(hex):
@@ -106,7 +109,7 @@ def get_available_models():
     """
 
     # sending get request
-    URL = st.session_state['API_URL'] + "/get_available_models"
+    URL = st.session_state["API_URL"] + "/get_available_models"
     r = requests.get(url=URL)
 
     # extracting data in json format
@@ -144,7 +147,7 @@ def post_images(uploaded_files, selected_model):
     print(files)
 
     # sending get request
-    URL = st.session_state['API_URL'] + "/predict_defects"
+    URL = st.session_state["API_URL"] + "/predict_defects"
 
     r = requests.post(
         URL,
@@ -157,17 +160,201 @@ def post_images(uploaded_files, selected_model):
     return data
 
 
+def submit_images(files, selected_model, show_labels, show_jsons, show_json):
+    """
+    Prepare the UploadedFiles for the 'predict_draw_defects' function
+
+    Parameters
+    ----------
+    files : list
+        A list of files to use for prediction and drawing.
+    selected_model : str
+        The model_id of the model to use for inference.
+    show_labels : boolean
+        An option to display defect labels on the images.
+    show_jsons : boolean
+        An option to display defect characteristics next to each image.
+    show_json : boolean
+        An option to display the JSON returned by the API at the end of the call.
+    """
+
+    if len(files) > 0:
+        predict_draw_defects(
+            files,
+            selected_model,
+            show_labels,
+            show_jsons,
+            show_json,
+        )
+
+
+def submit_video(uploaded_video, selected_model, show_labels, show_jsons, show_json):
+    """
+    Prepare the uploaded video for the 'predict_draw_defects' function
+
+    Parameters
+    ----------
+    uploaded_video : UploadedFile
+        A video file to use for prediction and drawing.
+    selected_model : str
+        The model_id of the model to use for inference.
+    show_labels : boolean
+        An option to display defect labels on the images.
+    show_jsons : boolean
+        An option to display defect characteristics next to each image.
+    show_json : boolean
+        An option to display the JSON returned by the API at the end of the call.
+    """
+
+    # --- CAPTURE VIDEO STREAM ---
+
+    vid = uploaded_video.name
+    with open(vid, mode="wb") as f:
+        f.write(uploaded_video.read())  # save video to disk
+
+    vidcap = cv2.VideoCapture(vid)  # load video from disk
+
+    # --- LOOP OVER CAPTURED FRAMES ---
+
+    # vid_div = st.empty()
+    cur_frame = 0
+    success = True
+    frame_skip = 300  # display every 300 frames
+
+    while success:
+        success, frame = vidcap.read()  # get next frame
+        if cur_frame % frame_skip == 0:  # draw every x frames
+            # pil_img = Image.fromarray(frame) # convert cv2 frame to PIL Image
+
+            is_success, buffer = cv2.imencode(".png", frame)
+            io_buf = io.BytesIO(buffer)
+            io_buf.name = "capture.png"
+
+            captured_files = [io_buf]
+            predict_draw_defects(
+                captured_files, selected_model, show_labels, show_jsons, show_json
+            )
+            # vid_div.image(pil_img)
+            # vid_div.image(io_buf.getvalue())
+
+        cur_frame += 1
+
+
+def predict_draw_defects(files, selected_model, show_labels, show_jsons, show_json):
+    """
+    Predict defects using the provided images and parameters
+
+    Parameters
+    ----------
+    files : list
+        A list of files to use for prediction and drawing.
+    selected_model : str
+        The model_id of the model to use for inference.
+    show_labels : boolean
+        An option to display defect labels on the images.
+    show_jsons : boolean
+        An option to display defect characteristics next to each image.
+    show_json : boolean
+        An option to display the JSON returned by the API at the end of the call.
+    """
+    json = post_images(files, selected_model)
+    defects = json["defects"]
+
+    for file in files:
+        image = Image.open(file)
+        image = image.convert("RGB") # remove alpha if any
+        image = image.save("img.jpg")
+
+        # Reading an image in default mode
+        image = cv2.imread("img.jpg")
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # Window name in which image is displayed
+        window_name = "Image"
+
+        # Line thickness of 2 px
+        thickness = 3
+        font_scale = 1.5
+
+        defects_str = []
+        count = 0
+        for defect in defects:
+            if file.name != defect["file"]:
+                continue
+
+            # defects_str.append((defect["type"], defect["probability"]))
+            defects_str.append(
+                {k: v for k, v in defect.items() if k in ["type", "probability"]}
+            )
+
+            # Get defect coords
+            x, y, w, h = defect["coords"]
+
+            # Blue color in BGR
+            dtype = defect["type"]
+            color = hex_to_rgb(colors[dtype])
+            # color = (255, 0, 0)
+
+            # Start coordinate (the top left corner of rectangle)
+            start_point = (int(x), int(y))
+
+            # Ending coordinate (represents the bottom right corner of rectangle)
+            end_point = (int(w), int(h))
+
+            # Draw rectangle around the defect
+            image = cv2.rectangle(image, start_point, end_point, color, thickness)
+
+            # Add label next to the defect
+            if show_labels:
+                txt = f"{count} {dtype}"
+            else:
+                txt = f"{count}"
+
+            image = draw_text(
+                image,
+                txt,
+                font=cv2.FONT_HERSHEY_DUPLEX,
+                pos=start_point,
+                font_scale=font_scale,
+                font_thickness=thickness,
+                text_color=color,
+                text_color_bg=(255, 255, 255),
+            )
+
+            count += 1
+
+        st.image(image)
+        # st.json(defects_str, expanded=False)
+        defects_expander = st.expander("Defects", expanded=show_jsons)
+        defects_expander.write(defects_str)
+
+    st.write(
+        "**Model used:**",
+        json["defect_model"],
+        "||",
+        "**Total inference time:**",
+        json["inference_time"],
+        " || ",
+        "**Mean inference time:**",
+        json["mean_inference_time"],
+    )
+
+    json_expander = st.expander("Returned JSON", expanded=show_json)
+    json_expander.write(json)
+
+# ===== LAYOUTS =====
+
 def init():
     """
-    Request the API base url
+    Display a layout to request the base API URL
     """
 
     def go(base_url):
         available_models, current_index = get_available_models()
         main(base_url, available_models, current_index)
 
-    if 'API_URL' in st.session_state:
-        go(st.session_state['API_URL'])
+    if "API_URL" in st.session_state:
+        go(st.session_state["API_URL"])
 
     else:
         form_div = st.empty()
@@ -177,7 +364,7 @@ def init():
 
         if submitted and base_url != "":
             try:
-                st.session_state['API_URL'] = base_url
+                st.session_state["API_URL"] = base_url
                 go(base_url)
                 form_div.empty()
             except Exception:
@@ -187,158 +374,57 @@ def init():
 
 def main(base_url, available_models, current_index):
     """
-    Define the streamlit app layout and base actions
+    Display a layout to actually select medias, send them to the API and display the results
     """
 
-    st.sidebar.write("API_URL:", st.session_state['API_URL'])
-
-    # --- Side bar ---
+    st.sidebar.write("API_URL:", st.session_state["API_URL"])
 
     form = st.sidebar.form("pred_form")
 
-    option = form.selectbox(
+    # --- FORM OPTIONS ---
+
+    model_id = form.selectbox(
         label="Select a model",
         options=available_models,
         index=current_index,
     )
 
-    show_labels = form.checkbox("Show labels on pictures", True)
+    show_labels = form.checkbox("Show labels on pictures", False)
     show_jsons = form.checkbox("Show defait details", False)
     show_json = form.checkbox("Show the whole JSON", False)
 
-    uploaded_files = form.file_uploader("Choose image(s)", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
-    submitted = form.form_submit_button("Submit")
+    # --- FORM TABS ---
 
-    if submitted:
-        if len(uploaded_files) > 0:
-            for uploaded_file in uploaded_files:
-                print("TYPE:", uploaded_file.type)
-            predict_and_draw(uploaded_files, available_models[option], show_labels, show_jsons, show_json)
+    tabs = form.tabs(["Use Image(s)", "Use Video"])
 
-    # --------------------
+    uploaded_files = tabs[0].file_uploader(
+        "Choose image(s)", type=["jpg", "jpeg", "png"], accept_multiple_files=True
+    )
+    submitted_i = tabs[0].form_submit_button("Submit image(s)")
 
-    form_v = st.sidebar.form("pred_formi_video")
-    uploaded_video = form_v.file_uploader("Choose video", type=["mp4", "mov"])
-    submitted_v = form_v.form_submit_button("Submit")
-    frame_skip = 300 # display every 300 frames
+    uploaded_video = tabs[1].file_uploader("Choose video", type=["mp4", "mov"])
+    submitted_v = tabs[1].form_submit_button("Submit video")
 
-    if submitted_v and uploaded_video is not None: # run only when user uploads video
-        vid = uploaded_video.name
-        with open(vid, mode='wb') as f:
-            f.write(uploaded_video.read()) # save video to disk
-    
-        st.markdown(f"""
-        ### Files
-        - {vid}
-        """,
-        unsafe_allow_html=True) # display file name
-    
-        vidcap = cv2.VideoCapture(vid) # load video from disk
-        cur_frame = 0
-        success = True
+    # --- FORM SUBMITS ---
 
-        vid_div = st.empty()
-    
-        while success:
-            success, frame = vidcap.read() # get next frame from video
-            if cur_frame % frame_skip == 0: # only analyze every n=300 frames
-                print('frame: {}'.format(cur_frame))
-                # pil_img = Image.fromarray(frame) # convert opencv frame (with type()==numpy) into PIL Image
+    if submitted_i:
+        submit_images(
+            uploaded_files,
+            available_models[model_id],
+            show_labels,
+            show_jsons,
+            show_json,
+        )
 
-                is_success, buffer = cv2.imencode(".jpg", frame)
-                io_buf = io.BytesIO(buffer)
-                io_buf.name = "Blop2.png"
-
-                #with open("Blop.png", "wb") as f:
-                #    f.write(io_buf.getvalue())
-
-                ups = [io_buf]
-                predict_and_draw(ups, available_models[option], show_labels, show_jsons, show_json)
-                # vid_div.image(pil_img)
-                # vid_div.image(io_buf.getvalue())
-            cur_frame += 1
-
-    # --- Center ---
-    # st.text("Hello world")
-
-
-def predict_and_draw(uploaded_files, selected_model, show_labels, show_jsons, show_json):
-
-    json = post_images(uploaded_files, selected_model)
-    print(json)
-    defects = json["defects"]
-    
-    for file in uploaded_files:
-        print("D01", type(file))
-        image = Image.open(file)
-        image = image.save("img.jpg")
-        print("D02", type(file))
-    
-        # Reading an image in default mode
-        image = cv2.imread("img.jpg")
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    
-        # Window name in which image is displayed
-        window_name = "Image"
-    
-        # Line thickness of 2 px
-        thickness = 1
-    
-        defects_str = []
-        count = 0
-        for defect in defects:
-            if file.name != defect["file"]:
-                continue
-    
-            # defects_str.append((defect["type"], defect["probability"]))
-            defects_str.append({ k:v for k, v in defect.items() if k in ['type', 'probability']})
-    
-            # Get defect coords
-            x, y, w, h = defect["coords"]
-    
-            # Blue color in BGR
-            dtype = defect["type"]
-            color = hex_to_rgb(colors[dtype])
-            # color = (255, 0, 0)
-    
-            # Start coordinate (the top left corner of rectangle)
-            start_point = (int(x), int(y))
-    
-            # Ending coordinate (represents the bottom right corner of rectangle)
-            end_point = (int(w), int(h))
-    
-            # Draw rectangle around the defect
-            image = cv2.rectangle(
-                image, start_point, end_point, color, thickness
+    if submitted_v:
+        if uploaded_video is not None:
+            submit_video(
+                uploaded_video,
+                available_models[model_id],
+                show_labels,
+                show_jsons,
+                show_json,
             )
-    
-            # Add label next to the defect
-            if show_labels:
-                txt = f"{count} {dtype}"
-            else:
-                txt = f"{count}"
-    
-            image = draw_text(
-                image,
-                txt,
-                font=cv2.FONT_HERSHEY_DUPLEX,
-                pos=start_point,
-                font_scale=0.6,
-                font_thickness=thickness,
-                text_color=color,
-                text_color_bg=(255, 255, 255),
-            )
-    
-            count +=1
-    
-        st.image(image)
-        # st.json(defects_str, expanded=False)
-        if show_jsons:
-            st.write(defects_str)
-
-    st.write("**Model used:**", json["defect_model"], "||", "**Total inference time:**", json["inference_time"], " || ", "**Mean inference time:**", json["mean_inference_time"])
-    if show_json:
-        st.write("JSON", json)
 
 if __name__ == "__main__":
     init()
